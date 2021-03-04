837cc16132a6588d78f43ead8e176f704be7eba7
Dag König <dagk@microsoft.com>
2021-03-04 13:39:17 +0100

    initial checkin

---
+++ src/OpcAlarmServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpcAlarmServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
