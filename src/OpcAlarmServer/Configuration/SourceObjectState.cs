1d74f71503b3104ad716fc6d454377483d070455
Dag König <dagk@microsoft.com>
2021-04-12 08:44:10 +0200

    fix: moved enum to separete file

---
+++ src/OpcAlarmServer/Configuration/SourceObjectState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpcAlarmServer.Configuration
+{
+    public enum SourceObjectState
+    {
+        BaseObjectState
+    }
+}
